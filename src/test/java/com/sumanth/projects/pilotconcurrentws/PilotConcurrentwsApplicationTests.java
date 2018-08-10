94a7aaf04b97085983af5e147a659f95a1470740
Sumanth Dwarakinath <sumanth.dwarakinath@gmail.com>
2018-08-10 01:12:37 -0600

    Initial commit

---
+++ src/test/java/com/sumanth/projects/pilotconcurrentws/PilotConcurrentwsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sumanth.projects.pilotconcurrentws;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PilotConcurrentwsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
